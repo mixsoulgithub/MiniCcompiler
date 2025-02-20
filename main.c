3d86197c36eff7ee0b01e770c027ef4aae0b2f36
wangzhan <wangzhan_1024@163.com>
2025-02-20 12:29:20 +0800

    support +-*/,()

--- main.c
+++ main.c
@@ -7,8 +7,6 @@ typedef enum {
     TK_PUNCT, // Punctuators
     TK_NUM,   // Numeric literals
     TK_EOF,   // End-of-file markers
-    TK_LEFTL, // Left parenthesis
-    TK_RIGHTL// Right parenthesis
   } TokenKind;
 
 typedef struct Token Token;
@@ -29,7 +27,7 @@ static Token* Tokenlize(char *p){
             p++;
             continue;
         }
-        if (*p == '+' || *p == '-') {
+        if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
             cur = cur->next = calloc(1, sizeof(Token));
             cur->kind = TK_PUNCT;
             cur->loc = p;
@@ -53,28 +51,141 @@ static Token* Tokenlize(char *p){
     return head.next;
 }
 
+typedef struct ASTnode ASTnode;
+struct ASTnode {
+    Token* tok;
+    ASTnode *left;
+    ASTnode *right;
+};
+
+static ASTnode* new_node(Token* tok, ASTnode *left, ASTnode *right){
+    ASTnode *node = calloc(1, sizeof(ASTnode));
+    node->tok = tok;
+    // if(tok->kind == TK_NUM){
+    //     printf("new node, val: %d\n", tok->val);
+    // }else{
+    //     printf("new node, loc: %s\n", tok->loc);
+    // }
+    node->left = left;
+    node->right = right;
+    return node;
+}
+
+
+//expr = mul ("+" mul | "-" mul)*
+static ASTnode* expr(Token **tok_addr);
+static ASTnode* mul(Token **tok_addr);
+static ASTnode* primary(Token **tok_addr);
+//tok is the first token of the expression.
+
+static ASTnode* expr(Token **tok_addr){
+    // printf("expr invoked\n");
+    ASTnode *node = mul(tok_addr);
+    Token *tok= *tok_addr;
+    while(tok->kind == TK_PUNCT && (*tok->loc == '+' || *tok->loc == '-')){
+        Token *op = tok;
+        tok = tok->next;
+        *tok_addr=tok;
+        // printf("tok accessed\n");
+        ASTnode *rhs = mul(tok_addr);
+        tok= *tok_addr;
+        node = new_node(op, node, rhs);//in this layer, node becomes left, so left-associative is guaranteed.
+    }
+    return node;
+}
+
+//mul = primary ("*" primary | "/" primary)*
+static ASTnode* mul(Token **tok_addr){
+    // printf("mul invoked\n");
+    ASTnode *node = primary(tok_addr);
+    Token *tok= *tok_addr;
+    while(tok->kind == TK_PUNCT && (*tok->loc == '*' || *tok->loc == '/')){
+        Token *op = tok;
+        tok = tok->next;
+        *tok_addr=tok;
+        ASTnode *rhs = primary(tok_addr);
+        tok= *tok_addr;
+        node = new_node(op, node, rhs);
+    }
+    return node;
+}
+
+//primary = num | "(" expr ")"  
+static ASTnode* primary(Token **tok_addr){
+    // printf("primary invoked\n");
+    Token *tok = *tok_addr;
+    if(tok->kind == TK_NUM){
+        ASTnode *node = new_node(tok, NULL, NULL);
+        tok = tok->next;
+        *tok_addr = tok;
+        // printf("tok accessed\n");
+        return node;
+    }
+    if(tok->kind == TK_PUNCT && *tok->loc == '('){
+        tok = tok->next;
+        *tok_addr = tok;
+        ASTnode *node = expr(tok_addr);
+        tok = *tok_addr;
+        if(!(tok->kind == TK_PUNCT && *tok->loc == ')')){
+            fprintf(stderr, "missing ')'\n");
+            exit(1);
+        }else{
+            tok = tok->next;
+            *tok_addr = tok;
+        }
+        return node;
+    }
+    fprintf(stderr, "unexpected token\n");
+    exit(1);
+}
+
+static ASTnode *ASTgen(Token *tok){
+    return expr(&tok);
+}
+
+static void codeGen(ASTnode *node){
+    if(node->tok->kind == TK_NUM && node->left == NULL && node->right == NULL){
+        printf("  mov $%d, %%rax\n", node->tok->val);
+        return;
+    }
+    codeGen(node->right);
+    printf("  push %%rax\n");
+    codeGen(node->left);
+    printf("  pop %%rdi\n");
+    if(*node->tok->loc == '+'){
+        printf("  add %%rdi, %%rax\n");
+    }else if(*node->tok->loc == '-'){
+        printf("  sub %%rdi, %%rax\n");
+    }else if(*node->tok->loc == '*'){
+        printf("  imul %%rdi, %%rax\n");
+    }else if(*node->tok->loc == '/'){
+        printf("  cqo\n");
+        printf("  idiv %%rdi\n");
+    }
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr,"Usage: %s <source code string>\n", argv[0]);
         return 1;
     }
     Token *tok = Tokenlize(argv[1]);
+    if (tok->kind == TK_EOF) {
+        fprintf(stderr, "no input\n");
+        return 1;
+    }
+    // while(tok->kind != TK_EOF){
+    //     if(tok->kind == TK_PUNCT){
+    //         printf("kind: TK_PUNCT, loc: %s\n", tok->loc);
+    //     }else if(tok->kind == TK_NUM){
+    //         printf("kind: TK_NUM, val: %d\n", tok->val);
+    //     }
+    //     tok = tok->next;
+    // }
     printf("  .globl main\n");
     printf("main:\n");
-    while(tok->kind != TK_EOF){//use eof rather than NULL
-        if(tok->kind == TK_NUM){
-            printf("  mov $%d, %%rax\n", tok->val);//in fact, it will excute one time if the input string is valid.
-        }else if(tok->kind == TK_PUNCT){//punct has only one character, easy to handle.
-            if(*tok->loc == '+'){
-                tok = tok->next;
-                printf("  add $%d, %%rax\n", tok->val);
-            }else if(*tok->loc == '-'){
-                tok = tok->next;
-                printf("  sub $%d, %%rax\n", tok->val);
-            }
-        } 
-        tok = tok->next;
-    }
+    ASTnode *node = ASTgen(tok);
+    codeGen(node);
     printf("  ret\n");
     return 0;
 }
--- test.sh
+++ test.sh
@@ -17,6 +17,13 @@ assert(){
     fi
 }
 
-assert 0 "12-3-5-4"
+# assert 42 "43-1"
+assert 0 "12-3-5-4" #left-assoc
+assert 0 "  12-3- 5- 4 " # space
+assert 12 "3*4" #mul
+assert 1 "3/2" #div
+assert 2 "3-(2- 1)" #paren
+assert 5 "1+2*3-4/2" #priority
+assert 4 "((1-1+3)/(2-1)-3)*2+4" #complex
 
 echo OK
