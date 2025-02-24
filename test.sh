166230f298db127aeab3a14b2502a1205f2e21f7
wangzhan <wangzhan_1024@163.com>
2025-02-24 21:41:54 +0800

    support ; and empty sentence

--- test.sh
+++ test.sh
@@ -17,36 +17,39 @@ assert(){
     fi
 }
 
-assert 42 "43-1"
-assert 0 "12-3-5-4" #left-assoc
-assert 0 "  12-3- 5- 4 " # space
-assert 12 "3*4" #mul
-assert 1 "3/2" #div
-assert 2 "3-(2- 1)" #paren
-assert 5 "1+2*3-4/2" #priority
-assert 4 "((1-1+3)/(2-1)-3)*2+4" #complex
-assert 2 "+2" #unary
-assert 3 "--+3"
-assert 6 "3++3"
-assert 1 "3+(-2)" 
-assert 1 "2<3"
-assert 0 '0==1'
-assert 1 '42==42'
-assert 1 '0!=1'
-assert 0 '42!=42'
+assert 42 "43-1;"
+assert 0 "12-3-5-4;" #left-assoc
+assert 0 "  12-3- 5- 4 ;" # space
+assert 12 "3*4;" #mul
+assert 1 "3/2;" #div
+assert 2 "3-(2- 1);" #paren
+assert 5 "1+2*3-4/2;" #priority
+assert 4 "((1-1+3)/(2-1)-3)*2+4;" #complex
+assert 2 "+2;" #unary
+assert 3 "--+3;"
+assert 6 "3++3;"
+assert 1 "3+(-2);" 
+assert 1 "2<3;"
+assert 0 '0==1;'
+assert 1 '42==42;'
+assert 1 '0!=1;'
+assert 0 '42!=42;'
 
-assert 1 '0<1'
-assert 0 '1<1'
-assert 0 '2<1'
-assert 1 '0<=1'
-assert 1 '1<=1'
-assert 0 '2<=1'
+assert 1 '0<1;'
+assert 0 '1<1;'
+assert 0 '2<1;'
+assert 1 '0<=1;'
+assert 1 '1<=1;'
+assert 0 '2<=1;'
 
-assert 1 '1>0'
-assert 0 '1>1'
-assert 0 '1>2'
-assert 1 '1>=0'
-assert 1 '1>=1'
-assert 0 '1>=2'
+assert 1 '1>0;'
+assert 0 '1>1;'
+assert 0 '1>2;'
+assert 1 '1>=0;'
+assert 1 '1>=1;'
+assert 0 '1>=2;'
+
+assert 3 '1;2;3;'
+assert 3 ';;3;'
 
 echo OK
