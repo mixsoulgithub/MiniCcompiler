c1d9001677948992f6fc2e316b74488063d9d245
wangzhan <wangzhan_1024@163.com>
2025-03-01 22:56:05 +0800

    support &,* and type check for +,- with ptr

--- test.sh
+++ test.sh
@@ -17,6 +17,7 @@ assert(){
     fi
 }
 
+assert 0 "{-1+1;}"
 # assert 42 "{43-1;}"
 # assert 0 "{12-3-5-4;}" #left-assoc
 # assert 0 "{  12-3- 5- 4 ;}" # space
@@ -26,9 +27,9 @@ assert(){
 # assert 5 "{1+2*3-4/2;}" #priority
 # assert 4 "{((1-1+3)/(2-1)-3)*2+4;}" #complex
 # assert 2 "{+2;}" #unary
-# assert 3 "{--+3;}"
+assert 3 "{--+3;}"
 # assert 6 "{3++3;}"
-# assert 1 "{3+(-2);}" 
+assert 1 "{3+(-2);}" 
 # assert 1 "{2<3;}"
 # assert 0 '{0==1;}'
 # assert 1 '{42==42;}'
@@ -64,10 +65,10 @@ assert(){
 # assert 8 '{foo123=3; bar=5;{;;}; return foo123+bar;}'
 
 # assert 1 '{{return 1;} 2; 3;}'
-# assert 2 '{1; return 2; 3;}'
+assert 2 '{ return 2;}'
 # assert 3 '{1; 2; return 3;}'
 
-# assert 3 '{ if (0) return 2; return 3; }'
+# assert 3 '{ if (0) return 2; }'
 # assert 3 '{ if (1-1==1) return 2; return 3; }'
 # assert 2 '{ if (1) return 2; return 3; }'
 # assert 2 '{ if (2-1) return 2; return 3; }'
@@ -76,7 +77,18 @@ assert(){
 # assert 2 '{ if (1) { a=1; } else { a=2; }; if (a==1) {a=2;}; return a; }'
 
 # assert 55 '{ i=0; j=0; for (i=0; i<=10; i=i+1) j=i+j; return j; }'
-assert 3 '{ for ( ; a<1 ; a=a+1) {return 3;} return 5; }'
-assert 89 '{ i=0;j1=0;j2=1; for (i=0; i<10; i=i+1) {tmp=j2;j2=j1+j2;j1=tmp;} return j2; }' #fibonacci
+# assert 3 '{ for ( ; a<1 ; a=a+1) {return 3;} return 5; }'
+# assert 89 '{ i=0;j1=0;j2=1; for (i=0; i<10; i=i+1) {tmp=j2;j2=j1+j2;j1=tmp;} return j2; }' #fibonacci
+# assert 10 '{ i=0; while(i<10) { i=i+1; } return i; }'
+
+# assert 3 '{ x=3; return *&x; }'
+# assert 3 '{ x=3; y=&x; z=&y; return **z; }'
+# assert 5 '{ x=3; y=5; return *(&x-1); }'
+# assert 3 '{ x=3; y=5; return *(&y+1); }'
+# assert 5 '{ x=3; y=5; return *(&x+-1); }'
+# assert 5 '{ x=3; y=&x; *y=5; return x; }'
+# assert 7 '{ x=3; y=5; *(&x-1)=7; return y; }'
+assert 7 '{ x=3; y=5; *(&y+2-1)=7; return x; }'
+assert 5 '{ x=3; return &x+2-&x+3; }'
 
 echo OK
---
+++ type.c
@@ -0,0 +1,74 @@
+#include "mini.h"
+void calcu_type(ASTnode *node);
+
+Type * ty_int = &(Type){.base=NULL,.tykind=TY_INT};//anonymous type_int.
+
+bool is_int(Type *ty){
+    return ty->tykind==TY_INT;
+}
+ 
+Type * point_to(Type*ty){
+    Type * new=calloc(1, sizeof(Type));
+    new->base=ty;
+    new->tykind=TY_POINTER;
+    return new;
+}
+
+Type* getNodeType(ASTnode*node){
+    if(!node){
+        fprintf(stderr,"<%s>: node can't be NULL",__func__);
+        exit(1);
+    }else{
+        calcu_type(node);
+        return node->type;
+    }
+}
+
+void calcu_type(ASTnode *node){
+    if(!node||node->type)
+        return;
+    calcu_type(node->left);
+    calcu_type(node->right);
+    calcu_type(node->cond);
+    calcu_type(node->inc);
+    calcu_type(node->init);
+    calcu_type(node->then);
+    calcu_type(node->els);
+    // calcu_type(node->next);//? do we need it? 
+    for(ASTnode* n=node->body;n;n=n->next){
+        calcu_type(n);
+    }
+    switch (node->kind)
+    {
+    case ND_ADD:
+    case ND_SUB:
+    case ND_MUL:
+    case ND_DIV:
+        node->type = node->left->type;
+        return;
+    case ND_NEG:
+    case ND_ASSIGN:
+        node->type = node->right->type;
+        return;
+    case ND_ADDR:
+        node->type=point_to(node->right->type);
+        return;
+    case ND_DEREF:
+        if(node->type->tykind==TY_POINTER){
+            node->type=node->right->type->base;
+        }else{
+            fprintf(stderr,"dereference of non-pointer");
+        }
+        return;
+    case ND_EQ:
+    case ND_NE:
+    case ND_LT:
+    case ND_LE:
+    case ND_VAR:
+    case ND_NUM:
+        node->type=ty_int;
+        
+    default:
+        return;
+    }
+}
